8724c074a9a2993b9c1b7071701e59028c958d5b
Sam Clark <sam@theclark.co.uk>
2011-01-22 16:03:21 +0000

    force close after saving a bookmark. Populates description field with selected text

--- engine.js
+++ engine.js
@@ -2,8 +2,8 @@ function openUrl(url) {
 	chrome.tabs.getAllInWindow(null, function (tabs) {
 		for (var i in tabs) {
 			var tab = tabs[i];
-			if (tab.url == url || tab.url == url.replace('http://', 'https://')) {
-				chrome.tabs.update(tab.id, {selected:true});								
+			if (tab.url == url || tab.url == url.replace("http://", "https://")) {
+				chrome.tabs.update(tab.id, {selected:true});	
 				close();
 				return;
 			}
@@ -14,46 +14,52 @@ function openUrl(url) {
 }
 
 function openOptions() {	
-	openUrl(chrome.extension.getURL('options.html'));
+	openUrl(chrome.extension.getURL("options.html"));
 }
 
 function openBookmarksPage(pageName) {
-	openUrl('http://pinboard.in/u:' + getOptionUserName() + '/' + pageName);			
+	openUrl("http://pinboard.in/u:" + getOptionUserName() + "/" + pageName);
 }
 
 function openAllBookmarks() {
-	openBookmarksPage('');
+	openBookmarksPage("");
 }
 
 function openPrivateBookmarks() {
-	openBookmarksPage('private/');
+	openBookmarksPage("private/");
 }
 
 function openPublicBookmarks() {
-	openBookmarksPage('public/');
+	openBookmarksPage("public/");
 }
 
 function openUnreadBookmarks() {
-	openBookmarksPage('unread/');
+	openBookmarksPage("unread/");
 }
 
 function openUntaggedBookmarks() {
-	openBookmarksPage('untagged/');
+	openBookmarksPage("untagged/");
 }
 
 function openStarredBookmarks() {
-	openBookmarksPage('starred/');
+	openBookmarksPage("starred/");
 }
 
 function saveBookmark() {
-	chrome.tabs.getSelected( null , function(tab) {
- 		window.open('http://pinboard.in/add?url=' + encodeURIComponent(tab.url) + '&title=' + encodeURIComponent(tab.title), 'pinboad.in', 'location=no,links=no,scrollbars=no,toolbar=no,width=700,height=350'); 		
-	});
+    chrome.tabs.getSelected(null , function(tab) {
+        if (tab.url.indexOf("http:") != 0 && tab.url.indexOf("https:") != 0) {
+            window.open("http://pinboard.in/add?jump=close&url=" + encodeURIComponent(tab.url) + "&title=" + encodeURIComponent(tab.title), "pinboad.in", "location=no,links=no,scrollbars=no,toolbar=no,width=700,height=350");
+        } else {
+            chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response) {
+                window.open("http://pinboard.in/add?jump=close&url=" + encodeURIComponent(tab.url) + "&title=" + encodeURIComponent(tab.title) + "&description=" + encodeURIComponent(response.data.substr(0, 256)), "pinboad.in", "location=no,links=no,scrollbars=no,toolbar=no,width=700,height=350");
+            });
+        }
+    });
 }
 
 function readLater() {
-	chrome.tabs.getSelected( null , function(tab) {
- 		window.open('http://pinboard.in/add?later=yes&noui=yes&jump=close&url=' + encodeURIComponent(tab.url) + '&title=' + encodeURIComponent(tab.title), 'pinboad.in', 'location=no,links=no,scrollbars=no,toolbar=no,width=0,height=0'); 		
+	chrome.tabs.getSelected(null , function(tab) {
+ 		window.open("http://pinboard.in/add?later=yes&noui=yes&jump=close&url=" + encodeURIComponent(tab.url) + "&title=" + encodeURIComponent(tab.title), "pinboad.in", "location=no,links=no,scrollbars=no,toolbar=no,width=0,height=0"); 		
 	});
 }
 
